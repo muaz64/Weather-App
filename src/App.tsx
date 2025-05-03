9f57c096bba1c43b27d0c46411317c9fbcfdea5b
muaz64 <muazmuhammad21@gmail.com>
2025-05-03 11:58:49 +0600

    app tsx update1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import './App.css'
+import SearchBar from './components/SearchBar'
 
 function App() {
   
 
   return (
-    <>
-      <div>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  
+      <div className="min-h-screen bg-gradient-to-br from-blue-200 to-blue-400 p-4">
+      <h1 className="text-3xl font-bold text-center text-white mb-6">
+        Weather App
+      </h1>
+      <SearchBar />
       </div>
       
-    </>
   )
 }
 
