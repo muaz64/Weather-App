1b3362b3a3c7bb031906b8d1f01270f6a4d963ef
muaz64 <muazmuhammad21@gmail.com>
2025-05-03 11:53:18 +0600

    SearchBar add .

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { useAppDispatch } from '../app/hooks';
+import { fetchWeather, setCity } from '../features/weatherSlice';
+
+const SearchBar = () => {
+    const [input, setInput] = useState('');
+    const dispatch = useAppDispatch();
+
+    const handleSearch = () => {
+        if (input.trim()) {
+            dispatch(setCity(input));
+            dispatch(fetchWeather(input));
+          }
+
+    };
+
+  return (
+    <div className='flex gap-2 justify-center mt-6'>
+        <input type="text" placeholder='Enter city' className='border rounded p-2 w-64' value={input} onChange={(e) => setInput (e.target.value)}/>
+        <button className='bg-blue-500 text-white px-4 py-2 rounded' onClick={handleSearch}>Search</button>
+    </div>
+  )
+}
+
+export default SearchBar
